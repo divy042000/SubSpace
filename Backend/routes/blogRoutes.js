11f6a98c1bc44e3d4f7038a27d79214aa59caf98
Patel Divy <divypatel0403@gmail.com>
2023-11-02 15:53:15 +0530

    Added the models and seted up the model for the payments

--- Backend/routes/blogRoutes.js
+++ Backend/routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const axios = require('axios');
-const router = express.Router();
+    const axios = require('axios');
+    const router = express.Router();
 
 const BlogController = require('../controllers/extractDetails');
 
